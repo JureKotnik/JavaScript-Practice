22b860531b193d7194343152bbf16a35e8bbd813
JureKotnik <jure2002k@gmail.com>
2023-01-25 17:17:54 +0100

    const

--- index.js
+++ index.js
@@ -61,3 +61,22 @@ console.log(z, typeof z);
 */
 
 ////////////////////////////////////////
+
+// Constant
+
+/*
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radious of the circle");
+radius = Number(radius);
+
+circumference = 2 * PI * radius;
+
+console.log("The circumference is:", circumference);
+*/
+
+////////////////////////////////////////
+
