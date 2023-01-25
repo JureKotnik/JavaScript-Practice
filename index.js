5795a4a9f857e53891cf8c6e5e111c7484174fab
JureKotnik <jure2002k@gmail.com>
2023-01-25 14:52:50 +0100

    Variables

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ console.log('I like pizza');
 
 window.alert('pizza');
 
+//Variabels
+
+let firstname ="Jure";
+let age = 20;
+let student = true;
+
+console.log( "Hello" , firstname);
+console.log("You are: " , age);
+console.log("Enroled: " , student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstname;
+document.getElementById("p2").innerHTML = "You are " + age + " Years old";
+document.getElementById("p3").innerHTML = "Enrolled: " + student;
+
+////////////////////////////////////////
