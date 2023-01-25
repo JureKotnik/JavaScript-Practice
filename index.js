3f170c1cae9a088f59591317ab2ebf5efda8db9f
JureKotnik <jure2002k@gmail.com>
2023-01-25 17:10:12 +0100

    type conversion

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-//console.log('I like pizza');
-
-//window.alert('pizza');
-
 //Variabels
+/*
 
 let firstname ="Jure";
 let age = 20;
@@ -16,9 +13,14 @@ document.getElementById("p1").innerHTML = "Hello " + firstname;
 document.getElementById("p2").innerHTML = "You are " + age + " Years old";
 document.getElementById("p3").innerHTML = "Enrolled: " + student;
 
+
+*/
+
 ////////////////////////////////////////
 
 //User input
+/*
+
 let username;
 document.getElementById("myButton").onclick = function(){
 
@@ -26,5 +28,36 @@ document.getElementById("myButton").onclick = function(){
     console.log(username);
     document.getElementById("myLabel").innerHTML = username;
 }
+
+*/
+
+
+////////////////////////////////////////
+
+//Type Conversion
+/*
+let age = window.prompt("How old are you?");
+
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+age += 1;
+
+console.log("Happy Birthday! You are", age, "years old");
+
+
+let x;
+let y;
+let z;
+
+x= Number("3.14");
+y = String(3.14);
+z = Boolean("");//if something is inside it wil show true else false
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+*/
 
 ////////////////////////////////////////
