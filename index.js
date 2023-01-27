15ea354ef95c6be1000cacabdfbf602b02cba0c6
JureKotnik <jure2002k@gmail.com>
2023-01-27 12:26:11 +0100

    counting program

--- index.js
+++ index.js
@@ -122,4 +122,27 @@ document.getElementById("submitButton").onclick = function(){
 ////////////////////////////////////////
 
 
+//counter program
+/*
+let count = 0;
+
+document.getElementById("decrease").onclick = function(){
+
+    count-=1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("reset").onclick = function(){
+
+    count = 0;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("increase").onclick = function(){
+
+    count+=1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+*/
+////////////////////////////////////////
 
