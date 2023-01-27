5240c94baa9346394332e941ec3fdf4f3ca19fac
JureKotnik <jure2002k@gmail.com>
2023-01-27 13:02:37 +0100

    slice method

--- index.js
+++ index.js
@@ -187,3 +187,20 @@ console.log(userName);
 console.log(phoneNumber);
 */
 ////////////////////////////////////////
+
+//slice method
+/*
+let fullName = "Jure Kotnik";
+let firstName;
+let lastName;
+
+//firstName = fullName.slice(0, 4);
+//lastName = fullName.slice(5);
+
+firstName = fullName.slice(0, fullName.indexOf(" "));
+lastName = fullName.slice(fullName.indexOf(" ") +1);
+
+console.log(firstName);
+console.log(lastName);
+*/
+////////////////////////////////////////
